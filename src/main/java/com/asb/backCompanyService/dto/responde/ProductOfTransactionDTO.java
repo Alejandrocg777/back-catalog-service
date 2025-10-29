f513154300ae70d7f179080898adfb242cfed59c
Alejandro Ceron <alejandrocg@switchs.co>
2025-10-29 13:34:48 -0500

    feat: maestro detalle de transacciones de entradas y salidas

---
+++ src/main/java/com/asb/backCompanyService/dto/responde/ProductOfTransactionDTO.java
@@ -0,0 +1,16 @@
+package com.asb.backCompanyService.dto.responde;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductOfTransactionDTO {
+
+    private Long ig;
+    private String productName;
+    private Double purchasePrice;
+    private Double total;
+}
