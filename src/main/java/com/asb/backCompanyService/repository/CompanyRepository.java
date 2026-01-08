d9112baae8fee392623d1cd4fe98e5e13d563c00
faridavila <farid.avila01@uceva.edu.co>
2026-01-08 00:15:30 -0500

    sintaxis posgrest

--- src/main/java/com/asb/backCompanyService/repository/CompanyRepository.java
+++ src/main/java/com/asb/backCompanyService/repository/CompanyRepository.java
@@ -12,17 +12,32 @@ public interface CompanyRepository extends JpaRepository<Company, Long> {
 
     List<Company> findByStatus(String status);
 
-    @Query(value = "SELECT c.company_id, c.`name_`, c.nit, c.address, c.email, c.phone, e.`description`, e.ciiu_code, c.economic_activity_id, c.image " +
+    //    @Query(value = "SELECT c.company_id, c.`name_`, c.nit, c.address, c.email, c.phone, e.`description`, e.ciiu_code, c.economic_activity_id, c.image " +
+//            "FROM company c " +
+//            "JOIN economic_activity e ON c.economic_activity_id = e.economic_activity_id " +
+//            "WHERE c.company_id = :id ", nativeQuery = true)
+//    List<Object[]> getCompanyIds(Long id);
+//
+//    @Query(value = "SELECT c.company_id, c.`name_`, c.nit, c.address, c.email, c.phone, e.`description`, e.ciiu_code, c.economic_activity_id " +
+//            "FROM company c " +
+//            "JOIN economic_activity e ON c.economic_activity_id = e.economic_activity_id " +
+//            "WHERE c.status = 'ACTIVE' ", nativeQuery = true)
+//    Page<Object[]> getStatus(Pageable pageable);
+
+//POSGRET
+    @Query(value = "SELECT c.company_id, c.name_, c.nit, c.address, c.email, c.phone, e.description, e.ciiu_code, c.economic_activity_id, c.image " +
             "FROM company c " +
             "JOIN economic_activity e ON c.economic_activity_id = e.economic_activity_id " +
             "WHERE c.company_id = :id ", nativeQuery = true)
     List<Object[]> getCompanyIds(Long id);
 
-    @Query(value = "SELECT c.company_id, c.`name_`, c.nit, c.address, c.email, c.phone, e.`description`, e.ciiu_code, c.economic_activity_id " +
+    @Query(value = "SELECT c.company_id, c.name_, c.nit, c.address, c.email, c.phone, e.description, e.ciiu_code, c.economic_activity_id " +
             "FROM company c " +
             "JOIN economic_activity e ON c.economic_activity_id = e.economic_activity_id " +
-            "WHERE c.status = 'ACTIVE' " , nativeQuery = true)
+            "WHERE c.status = 'ACTIVE' ", nativeQuery = true)
     Page<Object[]> getStatus(Pageable pageable);
+
+
 
 
     boolean existsById(Long id);
