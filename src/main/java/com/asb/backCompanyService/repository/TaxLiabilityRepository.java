ce1171da1b0958efd0fbc1b65a1d4314964cf4f1
faridavila <farid.avila01@uceva.edu.co>
2026-01-07 22:01:41 -0500

    prueba

--- src/main/java/com/asb/backCompanyService/repository/TaxLiabilityRepository.java
+++ src/main/java/com/asb/backCompanyService/repository/TaxLiabilityRepository.java
@@ -4,4 +4,5 @@ import com.asb.backCompanyService.model.TaxLiability;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaxLiabilityRepository extends JpaRepository<TaxLiability, Long> {
+
 }
